e4a1937e15dbbc0487ab7384a212b13998044628
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-07 02:43:45 +0900

    [Update]: #7 deviceの初期設定

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,10 +3,14 @@
     <v-app-bar color="deep-purple" dark>
       <v-toolbar-title class="ma-5">D&D.Sharing</v-toolbar-title>
       <v-btn class="ma-2">
-        新規登録
+        <router-link class="routerLink" to="/signup">
+          新規登録
+        </router-link>
       </v-btn>
       <v-btn class="ma-2">
-        ログイン
+        <router-link class="routerLink" to="/login">
+          ログイン
+        </router-link>
       </v-btn>
     </v-app-bar>
   </div>
@@ -20,3 +24,11 @@
     })
   }
 </script>
+
+<style>
+  .routerLink {
+    color: white;
+    text-decoration: none;
+    font-size: large;
+  }
+</style>
