f24ac53e730c2d7b512daf6c7df6f89a9075e3ff
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-07 02:41:56 +0900

    [Add: ] #7  新規登録・ログインフォーム導入 機能はまだ未実装

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login">
+    <h1>ログイン</h1>
+    <div>
+      <div>{{ this.message }}</div>
+      <input type="text" v-model="email" placeholder="NAME" /><br />
+      <input type="password" v-model="password" placeholder="PASSWORD" /><br />
+      <button v-on:click="login()">LOGIN</button>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="signup">
+    <h1>新規登録</h1>
+    <div>
+      <div>{{ this.message }}</div>
+      <input type="text" v-model="email" placeholder="ユーザー名" /><br />
+      <input type="password" v-model="password" placeholder="パスワード" /><br />
+      <button v-on:click="signup()">新規登録する</button>
+    </div>
+  </div>
+</template>
