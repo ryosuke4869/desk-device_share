2ba850a46800a804f5d16f78713485ea0786521c
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-08 19:15:17 +0900

    [update]:  #9 認証機能の実装

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        rescue_from ActiveRecord::RecordNotFound, with: :render_404
+        before_action :configure_permitted_parameters, if: :devise_controller?
+
+        def render_404
+                render status: 404, json: { message: "record not found." }
+        end
+
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
 end
