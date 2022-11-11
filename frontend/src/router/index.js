7959d81275e0374d79dc508d833555258d7a351d
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-12 05:50:44 +0900

    [update]: #11 signup pageのレイアウト修正 formのcomponent化によるsignup pageの修正

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Top from '../views/Top.vue';
 
 // deviceのimport
-import login from "../components/Login.vue";
-import signup from "../components/Signup.vue";
+import login from "../views/Login.vue";
+import signup from "../views/Signup.vue";
 
 const routes = [
   {
@@ -15,10 +15,6 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   //deviceのroutes
   {
---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <!-- <UserFormCard /> -->
+
+      <v-card-text>
+        <v-form>
+          <UserFormName />
+          <UserFormEmail />
+          <UserFormPassword />
+
+          <v-card-actions>
+            <v-btn class="info" color="white">新規登録</v-btn>
+          </v-card-actions>
+
+        </v-form>
+
+        <v-form v-model="isValid">
+          <v-btn
+            :disabled="!isValid"
+            block
+            color="blue"
+          >
+          登録する
+          </v-btn>
+
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import UserFormCard from '@/components/user/userFormCard.vue';
+import UserFormEmail from '@/components/user/userFormEmail.vue';
+import UserFormName from '@/components/user/userFormName.vue';
+import UserFormPassword from '@/components/user/userFormPassword.vue';
+
+export default {
+    data() {
+        return {
+            isValid: false,
+            params: { user: { name: "", email: "", password: "" } }
+        };
+    },
+    methods: {
+        signUp() { }
+    },
+    components: { UserFormEmail, UserFormPassword, UserFormName, UserFormCard }
+}
+</script>
+
