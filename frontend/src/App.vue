2eebe04e8492289a4e27d6724ef62c01a05038e6
{ryosuke4869} <{ryo-62-gnt-0000@outlook.jp}>
2022-11-12 03:51:39 +0900

    [Add]: #11 vuexの導入 vuex導入に伴い、store.jsの作成と動作確認のためにcomponents作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@
         <router-link to="/about">About</router-link>
       </nav>
       <router-view/>
+      <HelloWorld />
     </v-main>
     <Footer />
   </v-app>
@@ -14,15 +15,17 @@
 
 <script>
 // Components
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
     Header,
-    Footer
+    Footer,
+    HelloWorld
   },
   computed: {
     isAuthenticated () { /*ログインしていたらtrueを返す*/ }
